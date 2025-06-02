b761f926da8d64dd6f9a98403b0d29d2c7cf561b
AMQL3it <atiqurrahmanrasel19@gmail.com>
2025-06-02 17:59:39 +0600

    Bug - Registration massage issue fixed

--- src/modules/auth/index.js
+++ src/modules/auth/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("./controller");
+const userValidator = require("../user/middleware");
+const dataFilterHandler = require("../../common/dataFilterHandler");
 
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  userValidator(false),
+  dataFilterHandler,
+  authController.register
+);
 router.post("/login", authController.login);
 router.post("/otp/send", authController.otpSend);
 router.post("/otp/verify", authController.otpVerify);
